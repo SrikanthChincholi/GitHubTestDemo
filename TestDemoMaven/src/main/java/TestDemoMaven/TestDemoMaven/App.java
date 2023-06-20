57666adda7bf41a1814b1e610669c32500de80fe
srikanthchincholi <srikanthchincholi@yahoo.com>
2023-06-20 10:05:34 +0530

    added files

---
+++ TestDemoMaven/src/main/java/TestDemoMaven/TestDemoMaven/App.java
@@ -0,0 +1,9 @@
+package TestDemoMaven.TestDemoMaven;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+  }
+}
---
+++ TestDemoMaven/src/test/java/TestDemoMaven/TestDemoMaven/AppTest.java
@@ -0,0 +1,13 @@
+package TestDemoMaven.TestDemoMaven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+	  System.out.println("This is a Maven Project ");
+    Assert.assertTrue(true);
+  }
+}
